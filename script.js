15dea28bdfb94a8a80455bcce899986adea805f1
LuxDivina444 <bjorn.bjornski123@gmail.com>
2024-09-24 07:54:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('shop-now-btn').addEventListener('click', function() {
+    alert('Redirecting to the shop!');
+});
+
+document.getElementById('join-community-btn').addEventListener('click', function() {
+    alert('Welcome to our cosmic community!');
+});
